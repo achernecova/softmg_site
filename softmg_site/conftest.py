59c7e8f7098074e62976d3da68ee5c4121be41fe
achernecova <achernecova1904@gmail.com>
2026-02-10 01:40:20 +0300

    SOFTMG-486: исправила conftest.py для запуска в CI

--- softmg_site/conftest.py
+++ softmg_site/conftest.py
@@ -1,38 +1,60 @@
+import os
 import random
 
 import pytest
+from dotenv import load_dotenv
 from faker import Faker
 from selene import browser
-from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 from softmg_site.api_helper.api_help import ApiHelper
 from softmg_site.utils import attach
 from softmg_site.utils.logger import setup_logger
 
+DEFAULT_BROWSER_VERSION = 127.0
 
-@pytest.fixture()
-def driver_setup_all(scope="session", autouse=True):
-    """Фикстура для запуска локально"""
-    chrome_options = Options()
-    # chrome_options.add_argument("--headless")
-    # chrome_options.page_load_strategy = "none"
-    chrome_options.add_argument("--start-maximized")
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browserVersion",
+        help="Версия браузера в котором будут запущены тесты",
+        default="127.0",
+    )
 
-    # Todo - для firefox другие переменные передаются в расширение:
-    # chrome_options.add_argument("--width=1920")
-    # chrome_options.add_argument("--height=1080")
-    driver = webdriver.Chrome(options=chrome_options)
 
-    # Передаем драйвер в Selene
-    browser.config.driver = driver
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
 
-    yield driver
-    browser.driver.maximize_window()
+@pytest.fixture(scope="function", autouse=True)
+def driver(request):
+    _browserVersion = request.config.getoption("--browserVersion")
+    _browserVersion = (
+        _browserVersion if _browserVersion != "" else DEFAULT_BROWSER_VERSION
+    )
+    options = Options()
+    options.set_capability("browserName", "chrome")
+    options.set_capability("browserVersion", _browserVersion)
+    options.set_capability("pageLoadStrategy", "eager")
+    # options.add_argument("--window-size=1280,900")
+    options.add_argument("--start-maximized")
+    options.set_capability("selenoid:options", {"enableVNC": True, "enableVideo": True})
+
+    login = os.getenv("LOGIN")
+    password = os.getenv("PASSWORD")
+    host_selenoid = os.getenv("HOST")
+
+    browser.config.driver_remote_url = f"https://{login}:{password}@{host_selenoid}"
+    browser.config.driver_options = options
+    browser.config.timeout = 6
+
+    yield
+
+    # browser.driver.maximize_window()
     attach.add_screenshot(browser)
     attach.add_logs(browser)
     attach.add_html(browser)
-    # attach.add_video(browser)
+    attach.add_video(browser)
+
     browser.quit()
 
 
