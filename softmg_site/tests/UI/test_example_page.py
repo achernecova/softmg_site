2ffcf1a859ca2d210b05bb4d466740ba318a4b53
achernecova <achernecova1904@gmail.com>
2026-02-14 16:01:58 +0300

    Fix: добавила метку regression в тест test_count_card_example_in_page

--- softmg_site/tests/UI/test_example_page.py
+++ softmg_site/tests/UI/test_example_page.py
@@ -1,12 +1,14 @@
 from time import sleep
 
 import allure
+import pytest
 
 from softmg_site.pages.examples_page import ExamplePage
 
 
 @allure.description("SOFTMG-1145: Проверка отдачи 6 кейсов на странице кейсов")
 @allure.title("Проверка отдачи 6 кейсов на странице кейсов")
+@pytest.mark.regression
 def test_count_card_example_in_page(driver):
     with allure.step("Открываем страницу"):
         page = ExamplePage()
