58ecf4c95e0b92180689a00395de1327ec2d6e45
achernecova <achernecova1904@gmail.com>
2026-02-16 23:13:12 +0300

    изменение conftest.py

--- softmg_site/tests/conftest.py
+++ softmg_site/tests/conftest.py
@@ -1,13 +1,18 @@
 import os
+import random
 
 import pytest
 from dotenv import load_dotenv
+from faker import Faker
 from selene import browser
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 from softmg_site.utils import attach
 
-DEFAULT_BROWSER_VERSION = "127.0"
+DEFAULT_BROWSER_VERSION = 127.0
 
 
 def pytest_addoption(parser):
@@ -22,31 +27,38 @@ def pytest_addoption(parser):
 def load_env():
     load_dotenv()
 
-@pytest.fixture(scope="function", autouse=True)
+
+@pytest.fixture(scope="function")
 def driver(request):
     _browserVersion = request.config.getoption("--browserVersion")
-    _browserVersion = (
-        _browserVersion if _browserVersion != "" else DEFAULT_BROWSER_VERSION
-    )
+    _browserVersion = _browserVersion if _browserVersion != "" else DEFAULT_BROWSER_VERSION
+
     options = Options()
     options.set_capability("browserName", "chrome")
     options.set_capability("browserVersion", _browserVersion)
     options.set_capability("pageLoadStrategy", "eager")
-    # options.add_argument("--window-size=1280,900")
     options.add_argument("--start-maximized")
     options.set_capability("selenoid:options", {"enableVNC": True, "enableVideo": True})
 
-    login = os.getenv("LOGIN")
-    password = os.getenv("PASSWORD")
-    host_selenoid = os.getenv("HOST")
+    selenoid_url = os.getenv("SELENOID_URL")
+    print(f"SELENOID_URL: {selenoid_url}")
+    # Определяем - есть подключение к SELENOID или нет
+    if selenoid_url is not None:
+        login = os.getenv("LOGIN")
+        password = os.getenv("PASSWORD")
+        host_selenoid = os.getenv("SELENOID_URL")
 
-    browser.config.driver_remote_url = f"https://{login}:{password}@{host_selenoid}"
-    browser.config.driver_options = options
+        browser.config.driver_remote_url = f"https://{login}:{password}@{host_selenoid}"
+        browser.config.driver_options = options
+    else:
+        service = Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service, options=options)
+        browser.config.driver = driver
+
     browser.config.timeout = 6
 
-    yield
+    yield browser
 
-    # browser.driver.maximize_window()
     attach.add_screenshot(browser)
     attach.add_logs(browser)
     attach.add_html(browser)
@@ -54,26 +66,111 @@ def driver(request):
 
     browser.quit()
 
-#
-# @pytest.fixture(scope="function", autouse=True)
-# def driver_setup():
-#     chrome_options = Options()
-#     # chrome_options.add_argument("--headless")
-#     # chrome_options.page_load_strategy = "none"
-#     chrome_options.add_argument("--start-maximized")
-#
-#     # Todo - для firefox другие переменные передаются в расширение:
-#     # chrome_options.add_argument("--width=1920")
-#     # chrome_options.add_argument("--height=1080")
-#     driver = webdriver.Chrome(options=chrome_options)
-#
-#     # Передаем драйвер в Selene
-#     browser.config.driver = driver
-#
-#     yield driver
-#     browser.driver.maximize_window()
-#     attach.add_screenshot(browser)
-#     attach.add_logs(browser)
-#     attach.add_html(browser)
-#     # attach.add_video(browser)
-#     browser.quit()
+
+def pytest_collection_modifyitems(items):
+    for item in items:
+        item.add_marker(pytest.mark.all)
+
+
+    # TODO не придумала как перенести рандом и faker внутрь метода, без использования фикстур и фабрик.
+    #  Но и так чтобы параметризация осталась. Слишком громоздко получается.
+
+
+input_data_in_fields = pytest.mark.parametrize(
+    "name_field, input_data, text_error",
+    [
+        ("email", "           ", "Обязательное поле для заполнения"),
+        ("email_symbols", "dtgrarg%#$##@tgbt.ru", "Некорректный email-адрес"),
+        ("phone", random.randint(100, 999), "Некорректный номер телефона"),
+        ("name", Faker("ru_RU").text(max_nb_chars=500), "Максимум 256 символов"),
+    ],
+    ids=[
+        "input_space_in_email",
+        "input_symbols_in_email",
+        "input_three_characters_in_phone",
+        "exceeding_number_of_characters_in_name",
+    ],
+)
+
+menu_level_second_services = pytest.mark.parametrize(
+    "index, page_name",
+    [
+        (0, "it-autstaffing"),
+        (1, "outsource"),
+        (2, "development"),
+        (3, "application-development"),
+        (4, "razrabotka-ai"),
+        (5, "razrabotka-po"),
+        (6, "support"),
+        (7, "devops"),
+        (8, "promotion"),
+
+    ],
+    ids=[
+        "Autstaffing",
+        "Outsource",
+        "Development",
+        "Application Development",
+        "AI Development",
+        "Software Development",
+        "Support",
+        "Devops",
+        "Promotion",
+    ],
+)
+
+menu_level_third_development = pytest.mark.parametrize(
+    "index, page_name",
+    [
+        (0, "development_framework"),
+        (1, "development_corporate"),
+        (2, "shop"),
+        (3, "interactive"),
+        (4, "card"),
+        (5, "landing"),
+    ],
+    ids=[
+        "development_framework",
+        "development_corporate",
+        "development_shop",
+        "development_interactive",
+        "development_card",
+        "development_landing",
+    ],
+)
+
+first_level_menu = pytest.mark.parametrize(
+    "index, page_name",
+    [
+        (0, "uslugi"),
+        (1, "calculator"),
+        (2, "examples"),
+        (3, "about-company"),
+        (4, "contacts"),
+    ],
+    ids=["Services", "Calculator", "Examples", "Company Info", "Contacts"],
+)
+
+menu_level_third_application = pytest.mark.parametrize(
+    "index, page_name",
+    [
+        (6, "application_ios"),
+        (7, "application_android"),
+        (8, "development_backbone"),
+        (9, "application_mongodb"),
+        (10, "application_java"),
+        (11, "application_smartphone"),
+        (12, "javascript_react"),
+        (13, "react_native"),
+    ],
+    ids=[
+        "ios",
+        "android",
+        "backbone",
+        "mongodb",
+        "java",
+        "smartphone",
+        "javascript_react",
+        "react_native",
+    ],
+)
